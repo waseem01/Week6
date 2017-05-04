37406ade3f0516286df5a0222e8dae9bb8e79239
waseemm <wmohd@CHIC02QRGD3G8WN.sea.corp.expecn.com>
2017-05-03 21:22:03 -0500

    Milestone 2

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import MapKit
 
 class PhotoMapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
 
         // Do any additional setup after loading the view.
     }
--- Podfile
+++ Podfile
@@ -1,4 +1,11 @@
-platform :ios, '8.0'
-use_frameworks!
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
 
-pod 'AFNetworking'
+target 'Photo Map' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Wait-Less
+  pod 'AFNetworking'
+
+end
