efd7116903fcba9767623b9503078f5ee9b977e4
Karpaga Ganesh Patchirajan <karpagaganesh_patchirajan@intuit.com>
2017-05-03 21:36:43 -0500

    milestone 3

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 import MapKit
 
-class PhotoMapViewController: UIViewController {
+class PhotoMapViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
 
+    @IBOutlet weak var cameraButton: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1))
         mapView.setRegion(sfRegion, animated: false)
-
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(onTapCamera))
+        
+        cameraButton.addGestureRecognizer(tap)
+    
         // Do any additional setup after loading the view.
     }
 
@@ -35,6 +41,31 @@ class PhotoMapViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
+    
+    func onTapCamera(){
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = .photoLibrary
+        
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+//        dismiss(animated: true, completion: nil)
+        dismiss(animated: true) { 
+            self.performSegue(withIdentifier: "tagSegue", sender: self)
+        }
+    }
+
     
 
 }
